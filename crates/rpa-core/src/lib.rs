fa51efbb150cd1cbfe828fe8d52ac017152a0c23
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2025-12-30 12:35:39 +0500

    (fix): unresolved import due to accidental rename

--- crates/rpa-core/src/lib.rs
+++ crates/rpa-core/src/lib.rs
@@ -12,7 +12,7 @@ pub use activity_metadata::{
     ActivityCategory, ActivityMetadata, ColorCategory, PinConfig, PropertyDef, PropertyType,
 };
 pub use constants::{ActivityCategories, ActivityDefaults, UiConstants};
-pub use execution::{execute_project, get_timestamp};
+pub use execution::{execute_project_with_typed_vars, get_timestamp};
 pub use ir::{Instruction, IrBuilder, IrProgram};
 pub use node_graph::{
     Activity, BranchType, Connection, LogEntry, LogLevel, NanoId, Node, Project, ProjectFile,
